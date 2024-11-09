2ef8d8a9f995d5e6f4795c2aec5b0176e2596264
hi-lufz1 <latifufauzi@gmail.com>
2024-11-09 15:25:32 +0700

    "Added express-ejs-layouts, updated app.js to use layouts, modified contact and index ejs templates, and updated package.json and package-lock.json files."

--- app.js
+++ app.js
@@ -5,18 +5,32 @@ const app = express();
 const todoRoutes = require('./routes/tododb.js');
 require('dotenv').config();
 const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
 
 
 app.use(express.json());
 app.use('/todos',todoRoutes);
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index',{layout:'layouts/main-layout'});
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact',{layout:'layouts/main-layout'});
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 app.use((req, res) =>
